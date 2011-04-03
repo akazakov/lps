6fb0954b0fbb2fba4f802301322ccb2529a80e93
Tyomer <kazakov@gmail.com>
2011-04-02 19:17:39 -0700

    0.1.1 added buttons

--- src/com/tyoma/lps/HelloGoogleMaps.java
+++ src/com/tyoma/lps/HelloGoogleMaps.java
@@ -1,10 +1,16 @@
 package com.tyoma.lps;
 
+import android.view.View.OnClickListener;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.TextView;
+
 
 import com.google.android.maps.MapActivity;
 
-public class HelloGoogleMaps extends MapActivity {
+public class HelloGoogleMaps extends MapActivity implements OnClickListener {
+
 
 	@Override
 	protected boolean isRouteDisplayed() {
@@ -14,8 +20,21 @@ public class HelloGoogleMaps extends MapActivity {
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.main);
+	
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Button button = (Button)findViewById(R.id.button1);
+		if(button != null) {
+	        button.setOnClickListener(this);   
+		}
+	}
+	
+	public void onClick(View v) {
+		TextView tv = (TextView) findViewById(R.id.text);
+		if(tv != null) {
+			tv.setText("Hi Andy!");
+		}
 	}
 
 }
